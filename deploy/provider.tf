4c7b13eda394837594ead0fc7c25e9ba1254c80f
KosovGrigorii <72564996+KosovGrigorii@users.noreply.github.com>
2022-12-30 14:30:44 +0500

    Update provider.tf

--- deploy/provider.tf
+++ deploy/provider.tf
@@ -7,14 +7,14 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "y0_AgAAAABAXyrBAATuwQAAAADRYyRM84_OBp4jS3qzHVbVUIT2qlyLzlk"
+  token     = "<OAuth-token>"
   cloud_id  = local.cloud_id
   folder_id = local.folder_id
   zone      = local.zone
 }
 
 locals {
-  cloud_id  = "b1ge32nfqaa79q02sin5"
-  folder_id = "b1ghbrp8fpp59522per5"
+  cloud_id  = "<cloud identifier>"
+  folder_id = "<folder identifier>"
   zone      = "ru-central1-a"
 }
