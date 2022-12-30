3e550c916133409f1f2dcb4bdf3743dbc3dfbc2b
KosovGrigorii <72564996+KosovGrigorii@users.noreply.github.com>
2022-12-30 14:33:59 +0500

    Update update.sh

--- frontend/update.sh
+++ frontend/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 npm version patch;
-aws s3api delete-objects --endpoint-url https://storage.yandexcloud.net --bucket movies-website-b1ghbrp8fpp59522per5 --delete "$(aws s3api list-object-versions --endpoint-url https://storage.yandexcloud.net --bucket movies-website-b1ghbrp8fpp59522per5 --query '{Objects: Versions[].{Key: Key, VersionId: VersionId}}' --max-items 1000)";
-aws --endpoint-url=https://storage.yandexcloud.net s3 cp --recursive . s3://movies-website-b1ghbrp8fpp59522per5;
+aws s3api delete-objects --endpoint-url https://storage.yandexcloud.net --bucket ${MOVIES_WEBSITE_BUCKET} --delete "$(aws s3api list-object-versions --endpoint-url https://storage.yandexcloud.net --bucket ${MOVIES_WEBSITE_BUCKET} --query '{Objects: Versions[].{Key: Key, VersionId: VersionId}}' --max-items 1000)";
+aws --endpoint-url=https://storage.yandexcloud.net s3 cp --recursive . s3://${MOVIES_WEBSITE_BUCKET};
 
